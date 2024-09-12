862e007c894f217c5c124c1179944e5644353f8b
AslanAbdulrazagov <aslanabdulrazaqov496@gmail.com>
2024-09-12 20:02:42 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+namespace MVCProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+            app.UseRouting();
+            app.UseStaticFiles();
+
+            app.UseEndpoints(endpoints => endpoints.MapControllerRoute("default", "{controller=home}/{action=index}/{id?}"));
+
+            app.Run();
+        }
+    }
+}
